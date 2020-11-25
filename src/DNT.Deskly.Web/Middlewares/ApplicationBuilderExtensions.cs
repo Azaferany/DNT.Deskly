054c48d762f56b6f192c55aa19cb9256c7d82c6b
ali zaferany <a.zaferany79@hotmail.com>
2020-11-25 19:43:31 +0330

    35756454564644

---
+++ src/DNT.Deskly.Web/Middlewares/ApplicationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNT.Deskly.Web.Middlewares
+{
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Make sure you add this code BEFORE app.UseStaticFiles();,
+        /// otherwise the headers will not be applied to your static files.
+        /// </summary>
+        public static IApplicationBuilder UseContentSecurityPolicy(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ContentSecurityPolicyMiddleware>();
+        }
+
+    }
+}
