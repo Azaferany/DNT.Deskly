a1b0d878f41d20f879719e9af23faa6d1b53b64d
ali zaferany <a.zaferany79@hotmail.com>
2020-11-26 13:10:14 +0330

    v 1.0.3 add some f

---
+++ src/DNT.Deskly.Web/Caching/CustomTicketStoreExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DNT.Deskly.Web.Caching;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Caching.SqlServer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETCoreIdentitySample.IocConfig
+{
+    public static class CustomTicketStoreExtensions
+    {
+        public static IServiceCollection AddDistributSqlServerCache(
+            this IServiceCollection services, Action<SqlServerCacheOptions> setupAction)
+        {
+            // To manage large identity cookies
+            services.AddDistributedSqlServerCache(setupAction);
+            services.AddScoped<ITicketStore, DistributedCacheTicketStore>();
+            return services;
+        }
+        public static IServiceCollection AddDistributedMemoryCache(this IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddScoped<ITicketStore, MemoryCacheTicketStore>();
+            return services;
+
+        }
+    }
+}
