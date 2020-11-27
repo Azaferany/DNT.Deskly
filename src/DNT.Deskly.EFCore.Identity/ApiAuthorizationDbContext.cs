733ca3dc8b21966c33f8832468b5a5387d58308e
ali zaferany <a.zaferany79@hotmail.com>
2020-11-27 19:36:11 +0330

    v 1.0.9.9

--- src/DNT.Deskly.EFCore.Identity/ApiAuthorizationDbContext.cs
+++ src/DNT.Deskly.EFCore.Identity/ApiAuthorizationDbContext.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DNT.Deskly.EFCore.Context.Hooks;
@@ -15,11 +16,53 @@ using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.ApiAuthorization.IdentityServer
 {
+    public abstract class ApiAuthorizationDbContext<TUser> :
+        ApiAuthorizationDbContext<TUser, string>
+        where TUser : IdentityUser
+    {
+        public ApiAuthorizationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions, IEnumerable<IHook> hooks)
+            : base(options, operationalStoreOptions, hooks) { }
+    }
+    public abstract class ApiAuthorizationDbContext<TUser, TKey> :
+        ApiAuthorizationDbContext<TUser, TKey, IdentityUserClaim<TKey>, IdentityUserLogin<TKey>, IdentityUserToken<TKey>>
+        where TUser : IdentityUser<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public ApiAuthorizationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions, IEnumerable<IHook> hooks)
+            : base(options, operationalStoreOptions, hooks) { }
+    }
+    public abstract class ApiAuthorizationDbContext<TUser, TKey, TUserClaim, TUserLogin, TUserToken> :
+        ApiAuthorizationDbContext<TUser, IdentityRole<TKey>, TKey, TUserClaim, IdentityUserRole<TKey>, TUserLogin, IdentityRoleClaim<TKey>, TUserToken>
+        where TUser : IdentityUser<TKey>
+        where TKey : IEquatable<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>
+        where TUserLogin : IdentityUserLogin<TKey>
+        where TUserToken : IdentityUserToken<TKey>
+    {
+        public ApiAuthorizationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions, IEnumerable<IHook> hooks) 
+            : base(options, operationalStoreOptions, hooks) { }
+    }
     /// <summary>
     /// Database abstraction for a combined <see cref="DbContext"/> using ASP.NET Identity and Identity Server.
     /// </summary>
     /// <typeparam name="TUser"></typeparam>
-    public class ApiAuthorizationDbContext<TUser> : IdentityDbContext<TUser>, IPersistedGrantDbContext where TUser : IdentityUser
+    public abstract class ApiAuthorizationDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken> :
+    IdentityDbContext<TUser, TRole, TKey, TUserClaim, TUserRole, TUserLogin, TRoleClaim, TUserToken>,
+    IPersistedGrantDbContext
+    where TUser : IdentityUser<TKey>
+    where TRole : IdentityRole<TKey>
+    where TKey : IEquatable<TKey>
+    where TUserClaim : IdentityUserClaim<TKey>
+    where TUserRole : IdentityUserRole<TKey>
+    where TUserLogin : IdentityUserLogin<TKey>
+    where TRoleClaim : IdentityRoleClaim<TKey>
+    where TUserToken : IdentityUserToken<TKey>
     {
         private readonly IOptions<OperationalStoreOptions> _operationalStoreOptions;
 
@@ -30,9 +73,9 @@ namespace Microsoft.AspNetCore.ApiAuthorization.IdentityServer
         /// <param name="operationalStoreOptions">The <see cref="IOptions{OperationalStoreOptions}"/>.</param>
         public ApiAuthorizationDbContext(
             DbContextOptions options,
-            IOptions<OperationalStoreOptions> operationalStoreOptions, IEnumerable<IHook> hooks) : base(options, hooks) 
-        { 
-        _operationalStoreOptions = operationalStoreOptions;
+            IOptions<OperationalStoreOptions> operationalStoreOptions, IEnumerable<IHook> hooks) : base(options, hooks)
+        {
+            _operationalStoreOptions = operationalStoreOptions;
         }
 
         /// <summary>
---
+++ src/DNT.Deskly/Mvc/IMvcActionsDiscoveryService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DNT.Deskly.Mvc
+{
+    /// <summary>
+    /// More info: http://www.dotnettips.info/post/2573
+    /// </summary>
+    public interface IMvcActionsDiscoveryService
+    {
+        /// <summary>
+        /// Returns the list of all of the controllers and action methods of an MVC application.
+        /// </summary>
+        ICollection<MvcControllerViewModel> MvcControllers { get; }
+
+        /// <summary>
+        /// Returns the list of all of the controllers and action methods of an MVC application which have AuthorizeAttribute and the specified policyName.
+        /// </summary>
+        ICollection<MvcControllerViewModel> GetAllSecuredControllerActionsWithPolicy(string policyName);
+    }
+    /// <summary>
+    /// MvcController ViewModel
+    /// </summary>
+    public class MvcControllerViewModel
+    {
+        /// <summary>
+        /// Return `AreaAttribute.RouteValue`
+        /// </summary>
+        public string AreaName { get; set; }
+
+        /// <summary>
+        /// Returns the list of the Controller's Attributes.
+        /// </summary>
+        public IList<Attribute> ControllerAttributes { get; set; }
+
+        /// <summary>
+        /// Returns the `DisplayNameAttribute` value
+        /// </summary>
+        public string ControllerDisplayName { get; set; }
+
+        /// <summary>
+        /// It's set to `{AreaName}:{ControllerName}`
+        /// </summary>
+        public string ControllerId => $"{AreaName}:{ControllerName}";
+
+        /// <summary>
+        /// Return ControllerActionDescriptor.ControllerName
+        /// </summary>
+        public string ControllerName { get; set; }
+
+        /// <summary>
+        /// Returns the list of the Controller's action methods.
+        /// </summary>
+        public IList<MvcActionViewModel> MvcActions { get; set; } = new List<MvcActionViewModel>();
+
+        /// <summary>
+        /// Returns `[{controllerAttributes}]{AreaName}.{ControllerName}`
+        /// </summary>
+        public override string ToString()
+        {
+            const string attribute = "Attribute";
+            var controllerAttributes = string.Join(",", ControllerAttributes.Select(a => a.GetType().Name.Replace(attribute, "")));
+            return $"[{controllerAttributes}]{AreaName}.{ControllerName}";
+        }
+    }
+    /// <summary>
+    /// MvcAction ViewModel
+    /// </summary>
+    public class MvcActionViewModel
+    {
+        /// <summary>
+        /// Returns the list of Attributes of the action method.
+        /// </summary>
+        public IList<Attribute> ActionAttributes { get; set; }
+
+        /// <summary>
+        /// Returns `DisplayNameAttribute` value of the action method.
+        /// </summary>
+        public string ActionDisplayName { get; set; }
+
+        /// <summary>
+        /// It's set to `{ControllerId}:{ActionName}`
+        /// </summary>
+        public string ActionId => $"{ControllerId}:{ActionName}";
+
+        /// <summary>
+        /// Return ControllerActionDescriptor.ActionName
+        /// </summary>
+        public string ActionName { get; set; }
+
+        /// <summary>
+        /// It's set to `{AreaName}:{ControllerName}`
+        /// </summary>
+        public string ControllerId { get; set; }
+
+        /// <summary>
+        /// Returns true if the action method has an `AuthorizeAttribute`.
+        /// </summary>
+        public bool IsSecuredAction { get; set; }
+
+
+        /// <summary>
+        /// Returns `[{actionAttributes}]{ActionName}`
+        /// </summary>
+        public override string ToString()
+        {
+            const string attribute = "Attribute";
+            var actionAttributes = string.Join(",", ActionAttributes.Select(a => a.GetType().Name.Replace(attribute, "")));
+            return $"[{actionAttributes}]{ActionName}";
+        }
+    }
+
+}
